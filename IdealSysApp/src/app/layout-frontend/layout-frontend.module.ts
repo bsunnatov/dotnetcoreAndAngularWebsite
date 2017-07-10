75618928a163b298926e368bd318027a14c45726
Bobur Sunnatov <bsunnatov@gmail.com>
2017-07-10 19:01:25 +0500

    frontend-style

--- IdealSysApp/src/app/layout-frontend/layout-frontend.module.ts
+++ IdealSysApp/src/app/layout-frontend/layout-frontend.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { LayoutFrontendRoutingModule } from './layout-frontend-routing.module';
 import { LayoutFrontendComponent } from './layout-frontend.component';
 import { AboutComponent } from './about/about.component';
+import { TopbarComponent } from './topbar/topbar.component';
 @NgModule({
   imports: [
       CommonModule,
       LayoutFrontendRoutingModule,
     ],
-  declarations: [LayoutFrontendComponent, AboutComponent]
+  declarations: [LayoutFrontendComponent, AboutComponent, TopbarComponent]
 })
 export class LayoutFrontendModule { }
